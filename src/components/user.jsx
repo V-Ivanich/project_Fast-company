a824bba9fcf6218d23b1fdc48f9e9a700c06dafa
V-Ivanich <chipai_dom@mail.ru>
2022-11-07 15:19:34 +0300

    first string title

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,8 +1,32 @@
 import React,{ useState } from "react";
 import api from '../api'
 
+const titleTable = [
+  'Имя','Качества','Профессия','Встреч,кол-во','Оценка'
+]
+
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll())
+
+  const elementTH =()=>{
+    return (
+    titleTable.map((item) => (<th scope ="col">{item}</th>))
+    )
+  }
+
+  const createTable = () => {
+    return (
+      <>
+      <table className="table">
+        <thead>
+          <tr>
+            {elementTH()}
+          </tr>
+        </thead>
+      </table>
+      </>
+    )   
+  }
 
   const handleDelete =(userId) => {} //*-- кандидаты на гулянку(tabl)
 
@@ -10,6 +34,7 @@ const Users = () => {
 
   return (
       <>
+      {createTable()}
       </>
     )
 }
