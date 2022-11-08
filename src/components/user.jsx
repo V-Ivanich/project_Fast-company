79097eb9fb349fbe42795431f77e568c6f561dd7
V-Ivanich <chipai_dom@mail.ru>
2022-11-08 20:45:32 +0300

    full table, fix code

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -8,15 +8,8 @@ const Users = () => {
   const [keyItems, setKeyItems] = useState(["name","qualities","profession","completedMeetings","rate","bookmark"])
 
 
-
   const aparty = () =>{
     return <h3><span className="badge bg-primary mt-1 ms-1">12 человек на сундук мертвеца</span></h3>
-  }
-
-  const elementTH =()=>{
-    return (
-    titleItems.map((item, index) => (<th key={index} scope ="col">{item}</th>))
-    )
   }
 
   const rowTable = (index) => {
@@ -30,60 +23,35 @@ const Users = () => {
         )
     }
 
-    const nameUsersTable = () => {
-      return <td>{items.name}</td>
-    }
-
-    const profUsers = () => {
-      return <td>{items.profession.name}</td>
-    }
-
-    const rateUsers = () => {
-      return <td>{items.rate}</td>
-    }
-    const meetingsUsers = () => {
-      return <td>{items.completedMeetings}</td>
-    }
-
     return (
-      <tr>
-        {nameUsersTable()}
+      <tr className="text-center">
+        <td>{items.name}</td>
         <td>
           {buttonQualities()}
         </td>
-        {profUsers()}
-        {meetingsUsers()}
-        {rateUsers()}
+        <td>{items.profession.name}</td>
+        <td>{items.completedMeetings}</td>
+        <td>{items.rate}</td>
         <td>
-          <button type="button" className="btn btn-danger">delete</button>
+          <button type="button" className="btn btn-danger" onClick={handleDelete}>delete</button>
         </td>
       </tr>
     )
   }
-  
-  const tableRow = () => {
-    return (
-    <>
-      {rowTable(0)}
-    </>
-    )
-  }
-
 
   const createTable = () => {
     return (
       <>
-      <table className="table">
-        <thead className="table-light">
+      <table className="table table-striped table-hover">
+        <thead className="table-secondary">
           <tr>
-            {elementTH()}
+            {titleItems.map((item, index) => (<th key={index} className="text-center" scope ="col">{item}</th>))}
           </tr>
         </thead>
         <tbody>
-          {tableRow()}
+          {users.map((elem,index) => rowTable(index))}
         </tbody>
       </table>
-
       </>
     )   
   }
