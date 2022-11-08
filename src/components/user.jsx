f6d6daa9a03981658ff5696e1e5f9b1fb033b6d5
V-Ivanich <chipai_dom@mail.ru>
2022-11-08 14:17:49 +0300

    records one line

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -4,18 +4,14 @@ import api from '../api'
 
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll())
-  const [titleItems, setTitleItems] = useState(["Имя", "Качества", "Профессия","Встреч,кол-во","Оценка"]);
+  const [titleItems, setTitleItems] = useState(["Имя", "Качества", "Профессия","Встреч,кол-во","Оценка",""]);
   const [keyItems, setKeyItems] = useState(["name","qualities","profession","completedMeetings","rate","bookmark"])
 
-  console.log(users)
 
-  // const quli = ()=>{
-  //   mykeys.qualities.forEach(item => {
-  //     console.log(item.name, item.color)
-  //   })
-  // }
-  // quli()
 
+  const aparty = () =>{
+    return <h3><span className="badge bg-primary mt-1 ms-1">12 человек на сундук мертвеца</span></h3>
+  }
 
   const elementTH =()=>{
     return (
@@ -23,29 +19,48 @@ const Users = () => {
     )
   }
 
-
-
   const rowTable = (index) => {
     const items = users[index]
-    console.log(keyItems[0])
-    console.log(typeof items[keyItems[1]])
 
     const buttonQualities = () => {
-      const setClassName = "btn m-4 btn-"
+      const setClassName = "badge m-1 bg-"
       const elemQualitet = items.qualities
       return (
-          elemQualitet.map(elem => <button key={elem} type="button" className={setClassName + elem.color}>{elem.name}</button>)
+          elemQualitet.map(elem => <span key={elem._id} className={setClassName + elem.color}>{elem.name}</span>)
         )
     }
-    console.log(buttonQualities())
+
+    const nameUsersTable = () => {
+      return <td>{items.name}</td>
+    }
+
+    const profUsers = () => {
+      return <td>{items.profession.name}</td>
+    }
+
+    const rateUsers = () => {
+      return <td>{items.rate}</td>
+    }
+    const meetingsUsers = () => {
+      return <td>{items.completedMeetings}</td>
+    }
 
     return (
       <tr>
-        {buttonQualities()}
-        {/* {keyItems.map((itemKey) => (<td key={itemKey}>{keyItems}</td>))} */}
+        {nameUsersTable()}
+        <td>
+          {buttonQualities()}
+        </td>
+        {profUsers()}
+        {meetingsUsers()}
+        {rateUsers()}
+        <td>
+          <button type="button" className="btn btn-danger">delete</button>
+        </td>
       </tr>
     )
   }
+  
   const tableRow = () => {
     return (
     <>
@@ -59,7 +74,7 @@ const Users = () => {
     return (
       <>
       <table className="table">
-        <thead>
+        <thead className="table-light">
           <tr>
             {elementTH()}
           </tr>
@@ -79,6 +94,7 @@ const Users = () => {
 
   return (
       <>
+      {aparty()}
       {createTable()}
       </>
     )
