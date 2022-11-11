9d042399d8c623626ead2b7fb49dff5093c3197c
V-Ivanich <chipai_dom@mail.ru>
2022-11-11 14:37:33 +0300

    all is so goot

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -5,75 +5,72 @@ import api from '../api'
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll())
   const titleItems =["Имя", "Качества", "Профессия","Встреч,кол-во","Оценка",""]
-  const [countUsers, setCountUsers] = useState(users.length)
-  const [textContents, setTextContent] = useState(" человек тусанет с тобой сегодня")
-  const [messageAttribute, setMessageAttribute] = useState("badge mt-1 ms-1 bg-primary")
+
 
 const textMassage = () => {
-  let textStr = users.length ? users.length + ' ' + textContents : textContents
-  return (
-    <h3><span className={messageAttribute}>{textStr}</span></h3>
-    )
-  }
-
-  const rowTable = (index) => {
-    const items = users[index]
     return (
-      <tr key={items._id} className = "align-baseline">
-        <th scope = "row">{items.name}</th>
-        <td key={items.qualities._id}>
-          {items.qualities.map(elem => <span key={elem._id} className= {`badge rounded-pill px-3 py-2 m-1 bg-`+ elem.color}>{elem.name}</span>)}
-        </td>
-        <td>{items.profession.name}</td>
-        <td>{items.completedMeetings}</td>
-        <td>{items.rate}</td>
-        <td>
-          <button type="button" className="btn btn-danger rounded-pill" onClick={()=> handleDelete(items._id)}>delete</button>
-        </td>
-      </tr>
+      <h1 className={`badge rounded-pill mt-1 ms-1 px-2 py-2 bg-` + setAttribut()}>
+        {users.length !== 0 ? renderPhrase(users.length) : "Никто не пойдет с тобой тусить"}
+      </h1>
     )
   }
 
-  const createTable = () => {
+  const createTable = ()=> {
     if(users.length === 0) return
-    return  (
-      <>
+    return (
       <table className="table table-striped table-hover">
-        <thead className="table-secondary">
-          <tr key={users._id}>
-            {titleItems.map((item, index) => (<th key={index} className="ms-4" scope ="col">{item}</th>))}
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((elem,index) => rowTable(index))}
-        </tbody>
+      <thead className="table-secondary">
+        <tr key={users._id}>
+          {titleItems.map((item, index) => (<th key={index} className="ms-4" scope ="col">{item}</th>))}
+        </tr>
+      </thead>
+      <tbody>
+        {createTableBody()}
+      </tbody>
       </table>
-      </>
-    ) 
+    )
   }
-
+  
+  const setAttribut = ()=> {
+    return (users.length ? 'primary': 'danger')
+  }
+  const createTableBody = () => {
+    return users.map((user) => (
+        <tr key={user._id} className = "align-baseline">
+          <th scope = "row">{user.name}</th>
+          <td key={user.qualities._id}>
+            {user.qualities.map(elem => <span key={elem._id} className= {`badge rounded-pill px-3 py-2 m-1 bg-`+ elem.color}>{elem.name}</span>)}
+          </td>
+          <td>{user.profession.name}</td>
+          <td>{user.completedMeetings}</td>
+          <td>{user.rate}</td>
+          <td>
+            <button
+            type="button"
+            className="btn btn-danger rounded-pill"
+            onClick={()=> handleDelete(user._id)}
+            >
+            delete
+            </button>
+          </td>
+        </tr>
+      ))
+  }
+//!-----без стейта->!!!!!
   const handleDelete =(userId) => {
-    let activCount = countUsers
     setUsers(prevState => prevState.filter(user => user._id !== userId))
-    activCount -= 1
-    setCountUsers(countUsers -1)
-    renderPhrase(activCount)
+    console.log(users.length)
   }
 
+  //!----переделать без стейта->
   const renderPhrase = (number) => {
     let pattern = /^[2-4]{1}$|^.{0,}[2-4][2-4]$/
 
-
     if(pattern.test(number)){
-      setTextContent((prevState) => prevState= "человека тусанет с тобой сегодня")
+      return `${number} человека тусанет с тобой сегодня`
     } else {
-      setTextContent((prevState) => prevState = "человек тусанет с тобой сегодня")
+      return `${number} человек тусанет с тобой сегодня`
     }
-    if(number === 0) {
-      setTextContent((prevState) => prevState = "Никто не пойдет с тобой тусить")
-      setMessageAttribute((prevState) => prevState ="badge mt-1 ms-1 bg-danger")
-    }
-    return
   }
 
   return (
