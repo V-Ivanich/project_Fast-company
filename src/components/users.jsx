bf834a5566200f99f238191eabbb1e524ca5674d
V-Ivanich <chipai_dom@mail.ru>
2022-11-28 15:27:33 +0300

    new task

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pagination from "./pagination";
 import { paginate } from "../utils/paginate";
 import User from "./user";
+import GroupList from "./groupList";
 import PropTypes from "prop-types";
+import API from "../api";
 
 const titleItems = [
     "Имя",
@@ -18,14 +20,34 @@ const Users = ({ users, ...props }) => {
     const count = users.length;
     const pageSize = 4;
     const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState();
+    const [selectedProf, setSelectedProf] = useState();
+
+    useEffect(() => {
+        API.professions.fetchAll.then((data) => setProfessions(data));
+        console.log("currentPage");
+    }, []);
+
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
     };
+
+    const handleProfessionSelect = (item) => {
+        setSelectedProf(item);
+    };
+    console.log(professions);
 
     const userCrop = paginate(users, currentPage, pageSize);
 
     return (
         <>
+            {professions && (
+                <GroupList
+                    selectedItem={selectedProf}
+                    items ={professions}
+                    onItemSelect ={handleProfessionSelect}
+                />
+            )}
             {users.length > 0 && (
                 <table className="table table-striped table-hover align-middle">
                     <thead className="table-secondary">
