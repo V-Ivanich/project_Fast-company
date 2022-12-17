9ae6a65bd6e418deab2c4cb2521185163fedaec8
V-Ivanich <chipai_dom@mail.ru>
2022-12-17 22:58:57 +0300

    format?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const { off } = require("process");
-
 module.exports = {
   env: {
     browser: true,
@@ -13,46 +11,14 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    indent: ["error", 2],
-    semi: [2, "always"],
+    indent: ["error", 2], // Отступ количество пробелов
+    semi: [2, "always"], // Точка с запятой в конце строки
+    // Ошибка при наличии пробела при обозночении функции, уберём её
     "space-before-function-paren": [
       "error",
       { anonymous: "always", named: "never" }
     ],
+    // Использование двойных кавычек
     quotes: ["error", "double", { allowTemplateLiterals: true }]
   }
 };
-
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true
-//   },
-//   extends: ["plugin:react/recommended", "standard"],
-//   overrides: [],
-//   parserOptions: {
-//     ecmaFeatures: {
-//       jsx: true
-//     },
-//     ecmaVersion: "latest",
-//     sourceType: "module"
-//   },
-//   plugins: ["react"],
-//   rules: {
-//     // indent: ["error", 4], // Отступ количество пробелов
-//     semi: [2, "always"], // Точка с запятой в конце строки
-//     indent: "off",
-//     // "multiline-ternary": ["error", "always-multiline"],
-//     // "react/jsx-indent": "off",
-//     // "react/jsx-indent-props": "off",
-
-//     // Ошибка при наличии пробела при обозночении функции, уберём её
-//     "space-before-function-paren": [
-//       "error",
-//       { anonymous: "always", named: "never" }
-//     ],
-
-//     // Использование двойных кавычек
-//     quotes: ["error", "double", { allowTemplateLiterals: true }]
-//   }
-// };
