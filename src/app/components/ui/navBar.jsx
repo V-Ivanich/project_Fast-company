5f48fb6e392de9df73f02b26cc5a65d808d3e4ed
V-Ivanich <chipai_dom@mail.ru>
2023-02-27 22:11:41 +0300

    navbar

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 const NavBar = () => {
+    const { currentUser } = useAuth();
     return (
-        <>
+        <nav className="navbar">
             <div
-                className="container pl-3 mt-2"
+                className="container-fluid pl-3 mt-2"
                 style={{
                     background: "#dcdcdc",
                     borderRadius: "6px",
@@ -14,27 +16,37 @@ const NavBar = () => {
             >
                 <ul className="nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">
+                        <Link className="nav-link" aria-current="page" to="/">
                             Main
                         </Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">
-                            Login
-                        </Link>
-                    </li>
-                    <li className="nav-item">
+                    {currentUser && (
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link"
+                                aria-current="page"
+                                to="/users"
+                            >
+                                Users
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+                <div className="d-flex align-middle">
+                    {currentUser ? (
+                        <p>User</p>
+                    ) : (
                         <Link
                             className="nav-link"
                             aria-current="page"
-                            to="/users"
+                            to="/login"
                         >
-                            Users
+                            Login
                         </Link>
-                    </li>
-                </ul>
+                    )}
+                </div>
             </div>
-        </>
+        </nav>
     );
 };
 
