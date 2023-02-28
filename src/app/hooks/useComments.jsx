67092aa69dd64e45e8058dcf473f5b3defb858a5
V-Ivanich <chipai_dom@mail.ru>
2023-02-28 22:13:51 +0300

    comments write

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-// import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { nanoid } from "nanoid";
+import commentService from "../services/comment.service";
+import { toast } from "react-toastify";
 
 const CommentsContext = React.createContext();
 
@@ -9,19 +13,50 @@ export const useComments = () => {
 };
 
 export const CommentsProvider = ({ children }) => {
-    // const [isLoading, setLoading] = useState(true);
+    const { userId } = useParams();
+    const { currentUser } = useAuth();
+    const [isLoading, setLoading] = useState(true);
     const [comments, setComments] = useState([]);
-    // const [error, setError] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setComments(null);
+        setLoading(false);
     }, []);
 
     async function createComments(data) {
-        console.log(data);
+        const comment = {
+            ...data,
+            _id: nanoid(),
+            pageId: userId,
+            created_at: Date.now(),
+            userId: currentUser._id
+        };
+        try {
+            const { content } = await commentService.createComment(comment);
+            console.log(content);
+        } catch (error) {
+            errorCatcher();
+        }
     }
+
+    async function getComments() {}
+
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
     return (
-        <CommentsContext.Provider value={{ comments, createComments }}>
+        <CommentsContext.Provider
+            value={{ comments, createComments, isLoading }}
+        >
             {children}
         </CommentsContext.Provider>
     );
---
+++ src/app/services/comment.service.js
@@ -0,0 +1,19 @@
+import httpService from "./http.service";
+
+const commentEndpoint = "comment/";
+
+const commentService = {
+    createComment: async (payload) => {
+        const { data } = await httpService.put(
+            commentEndpoint + payload._id,
+            payload
+        );
+        return data;
+    },
+    getComments: async (pageId) => {
+        const { data } = await httpService.get(commentEndpoint);
+        return data;
+    }
+};
+
+export default commentService;
