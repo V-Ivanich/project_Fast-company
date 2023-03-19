8f8b36e9ed5325852de1e0985a38bde6c4634d37
V-Ivanich <chipai_dom@mail.ru>
2023-03-19 22:37:04 +0300

    add reducer

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import "./index.css";
 import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "./app/store/createStore";
+import { Provider } from "react-redux";
+
+const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>
 );
