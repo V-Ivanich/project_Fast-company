ac13ee7831d1a105dca9337b7b73480c263f28f2
V-Ivanich <chipai_dom@mail.ru>
2023-03-19 21:03:11 +0300

    fix changes user edit

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import EditUserPage from "../components/page/editUserPage/editUserPage";
 import UserProvider from "../hooks/useUsers";
+import { useAuth } from "../hooks/useAuth";
 
 const Users = () => {
     const { userId, edit } = useParams();
+    const { currentUser } = useAuth();
 
     return (
         <>
             <UserProvider>
                 {userId ? (
                     edit ? (
-                        <EditUserPage />
+                        userId === currentUser._id ? (
+                            <EditUserPage />
+                        ) : (
+                            <Redirect to={`/users/${currentUser._id}/edit`} />
+                        )
                     ) : (
                         <UserPage userId={userId} />
                     )
