5ad8ddad4ef039aa9408ef7620118cef20f1e939
V-Ivanich <chipai_dom@mail.ru>
2023-03-23 20:24:53 +0300

    user slice

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -1,8 +1,9 @@
 import { createAction, createSlice } from "@reduxjs/toolkit";
-import { rest } from "lodash";
 import authService from "../services/auth.service";
 import localStorageService from "../services/localStorage.service";
 import userService from "../services/user.service";
+import getRandomInt from "../utils/getRandomInt";
+import history from "../utils/history";
 
 const usersSlice = createSlice({
     name: "users",
@@ -30,6 +31,12 @@ const usersSlice = createSlice({
         },
         authRequestFaled: (state, action) => {
             state.error = action.payload;
+        },
+        userCreated: (state, action) => {
+            if (!Array.isArray(state.entities)) {
+                state.entities = [];
+            }
+            state.entities.push(action.payload);
         }
     }
 });
@@ -40,10 +47,13 @@ const {
     usersReceved,
     usersRequestFiled,
     authRequestSuccess,
-    authRequestFaled
+    authRequestFaled,
+    userCreated
 } = actions;
 
 const authRequested = createAction("users/authRequested");
+const userCreateRequested = createAction("users/userCreateRequested");
+const createUserFailed = createAction("users/createUserFailed");
 
 export const signUp =
     ({ email, password, ...rest }) =>
@@ -53,10 +63,37 @@ export const signUp =
             const data = await authService.register({ email, password });
             localStorageService.setToken(data);
             dispatch(authRequestSuccess({ userId: data.localId }));
+            dispatch(
+                createUser({
+                    _id: data.localId,
+                    email,
+                    rate: getRandomInt(1, 5),
+                    completedMeetings: getRandomInt(0, 200),
+                    image: `https://avatars.dicebear.com/api/avataaars/${(
+                        Math.random() + 1
+                    )
+                        .toString(36)
+                        .substring(7)}.svg`,
+                    ...rest
+                })
+            );
         } catch (error) {
             dispatch(authRequestFaled(error.message));
         }
     };
+
+function createUser(payload) {
+    return async function (dispatch) {
+        dispatch(userCreateRequested());
+        try {
+            const { content } = await userService.create(payload);
+            dispatch(userCreated(content));
+            history.push("/users");
+        } catch (error) {
+            dispatch(createUserFailed(error.message));
+        }
+    };
+}
 
 export const loadUsersList = () => async (dispatch, getState) => {
     dispatch(usersRequested());
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { createStore } from "./app/store/createStore";
 import { Provider } from "react-redux";
+import history from "./app/utils/history";
 
 const store = createStore();
 
@@ -13,10 +14,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <Router history={history}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
-        </BrowserRouter>
+        </Router>
     </Provider>
 );
