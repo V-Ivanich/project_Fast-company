ff5346ce0e775435349198be011c04566e2dec24
V-Ivanich <chipai_dom@mail.ru>
2023-03-31 20:37:37 +0300

    finish

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,6 @@ import Main from "./layouts/main";
 import Login from "./layouts/login";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import AuthProvider from "./hooks/useAuth";
 import ProtectedRoute from "./components/common/protectedRoute";
 import LogOut from "./layouts/logOut";
 import AppLoader from "./components/ui/hoc/appLoader";
@@ -14,19 +13,17 @@ const App = () => {
     return (
         <>
             <AppLoader>
-                <AuthProvider>
-                    <NavBar />
-                    <Switch>
-                        <ProtectedRoute
-                            path="/users/:userId?/:edit?"
-                            component={Users}
-                        />
-                        <Route path="/login/:type?" component={Login} />
-                        <Route path="/logout" component={LogOut} />
-                        <Route path="/" exact component={Main} />
-                        <Redirect to="/" />
-                    </Switch>
-                </AuthProvider>
+                <NavBar />
+                <Switch>
+                    <ProtectedRoute
+                        path="/users/:userId?/:edit?"
+                        component={Users}
+                    />
+                    <Route path="/login/:type?" component={Login} />
+                    <Route path="/logout" component={LogOut} />
+                    <Route path="/" exact component={Main} />
+                    <Redirect to="/" />
+                </Switch>
             </AppLoader>
             <ToastContainer />
         </>
